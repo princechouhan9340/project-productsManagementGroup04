c26345d5814a171fde8ca58784a13dc1c814a6d6
prince chouhan <pc75805@gmail.com>
2022-05-24 12:30:23 +0530

    index.js

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// IMPORT EXPRESS PACKAGE( COMMOM JS SYNTEX )------
+const express = require('express')
+
+// INSTATIATE THE EXPRESS APP------
+const app = express()
+
+// IMPORT EXPRESS PACKAGE( COMMOM JS SYNTEX )------
+const multer = require("multer")
+const { AppConfig } = require('aws-sdk');
+
+//IMPORT BODY PARESR PACKAGE-----
+const bodyParser = require('body-parser')
+
+// IMPORT ROUTE MODULE------
+const route = require('./routes/route');
+const mongoose = require('mongoose');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(multer().any())
+
+
+// DECLARE  A DATABASE STRING URL----
+mongoose.connect('"mongodb+srv://uranium-cohort:zxN697Vko486ved2@cluster0.23vax.mongodb.net/group4-DB"',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then( () => console.log("mongoDB is connected"))
+.catch ( err => console.log(err))
+
+// APP SET THEIR ENDPOINT IN ROUTE.JS----
+app.use('/',route)
+
+// LISTEN FOR INCOMING REQUESTS-----
+app.listen(process.env.PORT || 3000, function (){
+    console.log('Express app running on port' + (process.env.PORT || 3000))
+})
